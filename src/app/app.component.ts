f0ca5b46aa55bcd40ede63ce20e79663aa83f8f6
Зрячев Роман <zryachev@evolenta.ru>
2022-11-19 12:18:08 +0300

    practice 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Hello';
+    public title = 'Hello';
+    public date = new Date();
+    public names = [
+        {
+            name: 'Борис',
+        },
+        {
+            name: 'Владимир',
+        },
+        {
+            name: 'Дмитрий',
+        },
+        {
+            name: 'Владимир',
+        },
+        {
+            name: 'Владимир',
+        },
+    ];
 }
